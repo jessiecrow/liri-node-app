80239cd4f8e528253f10e9f1dccc31967f4d4ac3
Jessie Crow <jrmcrow@gmail.com>
2017-07-19 05:45:42 -0400

    changed keys

--- keys.js
+++ keys.js
@@ -1,14 +1,15 @@
 // JavaScript
 console.log('this is loaded');
 
-exports.twitterKeys = {
-  consumer_key: '6vasntj7nbSLNchtBG9GnK0Sj',
-  consumer_secret: 'Bx0D7ZktSd9RYaPyiYXk87NtepfnjSPexLUO2FXpT2l2Vq7hkP',
-  access_token_key: '886707888256888832-7DblBd3AQ6sgxp0vuXqGqC99Fe0aPoe',
-  access_token_secret: '8vLKFYHoCyJ3xN8eHCHdPR1l3LZmBZX6uC1FMPsCrmytu',
-};
-
-exports.spotifyKeys = {
-  client_id: '5bdcff11d3d04bc0a8a5ee9e19796d95',
-  client_secret:'4d6751cbddca416a88a688824facb5b5',
+exports.keys = {
+  twitter: {
+    consumer_key: '6vasntj7nbSLNchtBG9GnK0Sj',
+    consumer_secret: 'Bx0D7ZktSd9RYaPyiYXk87NtepfnjSPexLUO2FXpT2l2Vq7hkP',
+    access_token_key: '886707888256888832-7DblBd3AQ6sgxp0vuXqGqC99Fe0aPoe',
+    access_token_secret: '8vLKFYHoCyJ3xN8eHCHdPR1l3LZmBZX6uC1FMPsCrmytu',
+  },
+  spotify: {
+    client_id: '5bdcff11d3d04bc0a8a5ee9e19796d95',
+    client_secret:'4d6751cbddca416a88a688824facb5b5'
+  }
 };
